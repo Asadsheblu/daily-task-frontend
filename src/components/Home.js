9903ecb9b4bb50f3a45bf97912a6ce9b11f93cf7
Asadsheblu <shebluasad@gmail.com>
2022-07-01 11:46:19 +0600

    added

--- src/components/Home.js
+++ src/components/Home.js
@@ -32,7 +32,7 @@ const Home = () => {
           <form className='w-50 mx-auto' onSubmit={handelTask}>
           <div class="mb-3">
     <label for="exampleInputEmail1" class="form-label">Task Name</label>
-    <input
+    <input className='from-control'
           type="text"
           id="first"
           name="first"
@@ -44,7 +44,7 @@ const Home = () => {
   </div>
   <div class="mb-3">
     <label for="exampleInputPassword1" class="form-label">Task Date</label>
-    <input
+    <input className='from-control'
           type="date"
           id="first"
           name="duration"
@@ -54,15 +54,11 @@ const Home = () => {
         />
   </div>
   <div class="mb-3">
-    <label for="exampleInputPassword1" class="form-label">Task Details</label>
-    <input
-          type="text"
-          id="first"
-          name="details"
+    <label for="exampleInputPassword1" class="form-label">Task Details</label> <br />
+    <textarea className='from-control' id="first" cols="30" name="details"
           value={details}
-          onChange={event => setdetails(event.target.value)}
-          autoComplete="off"
-        />
+          onChange={event => setdetails(event.target.value)} rows="5"></textarea>
+   
   </div>
             <button className='btn btn-info'>Add Task</button>
           </form>
